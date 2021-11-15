5697ab009a9df1ce11124f481fcaff47814b5fe8
Bill Huang <bill.huang2001@gmail.com>
2021-11-15 21:39:10 +0800

    add much code

--- Node2D.gd
+++ Node2D.gd
@@ -1,15 +1,21 @@
 extends Node2D
 
-
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	var backgrounds = []
+	var dir = Directory.new()
+	dir.open('res://assets/Battle/Background/')
+	dir.list_dir_begin()
+	var file_name = dir.get_next()
+	while file_name != "":
+		if file_name.ends_with('png'):
+			backgrounds.append('res://assets/Battle/Background/' + file_name)
+		file_name = dir.get_next()
+	dir.list_dir_end ()
+	var bg_idx = randi() % len(backgrounds)
+	print(bg_idx)
+	$Background.set_texture(load(backgrounds[bg_idx]))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
